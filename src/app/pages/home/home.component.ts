42b146c5641831c016ef74c3c7be2c7dbcadbe8b
Valerio Pinsone <v.pinsone@almaviva.it>
2025-01-30 16:31:00 +0100

    aggiornamento interfaccia

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,30 +1,36 @@
-import { ChangeDetectionStrategy, Component, OnInit, OnDestroy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, OnDestroy, inject } from '@angular/core';
 import { ModbusService } from '../../services/modbus.service';
 import { FormControl, FormGroup, FormArray, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { Subscription, interval } from 'rxjs';
+import { Subscription, forkJoin, interval } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule, MatTableModule],
   changeDetection: ChangeDetectionStrategy.Default
 })
 export class HomeComponent implements OnInit, OnDestroy {
+  private snackBar = inject(MatSnackBar);
   ip = new FormControl('telecameretrend.controlliamo.com');
   port = new FormControl(502);
   slave = new FormControl(250);
-  registersForm = new FormGroup({
-    registers: new FormArray([
-      new FormControl(102),
-      new FormControl(1)
-    ])
-  });
+  registersInput: string = '';
+
+  registro = new FormControl();
+  valore = new FormControl();
+
   registers: { address: number, value: any }[] = [];
+
   private pollingSubscription: Subscription | undefined;
+
+  displayedColumns: string[] = ['registro', 'valore'];
+  dataSource: any[] = [];
 
   constructor(private modbusService: ModbusService) { }
 
@@ -37,22 +43,18 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.stopPolling();
   }
 
-  get registersArray() {
-    return this.registersForm.get('registers') as FormArray;
-  }
-
   addRegister() {
-    this.registersArray.push(new FormControl());
+    this.registers.push({ address: 0, value: 0 }); // Aggiungi un nuovo registro con valori predefiniti
   }
 
   removeRegister(index: number) {
-    this.registersArray.removeAt(index);
+    this.registers.splice(index, 1); // Rimuovi il registro all'indice specificato
   }
 
   startPolling() {
     this.pollingSubscription = interval(1000) // Polling ogni 1 secondo
       .pipe(
-        switchMap(() => this.loadRegisters())
+        switchMap(async () => this.loadRegisters())
       )
       .subscribe();
   }
@@ -63,26 +65,86 @@ export class HomeComponent implements OnInit, OnDestroy {
     }
   }
 
+  writeRegister() {
+    console.log('Writing register:', this.registro.value, this.valore.value);
+    this.modbusService.writeRegister(this.ip.value as any, this.port.value as any, this.slave.value as any, this.registro.value as any, this.valore.value as any)
+      .subscribe(response => {
+        console.log('Register written:', response);
+        if (response.success == true) {
+          this.snackBar.open("✅  "+response.message, 'Ok', {
+            duration: 3000
+          });
+          this.valore.setValue(null);
+        } else {
+          this.snackBar.open("❌  Errore: " + response.message, 'Ok', {
+            duration: 3000
+          });
+        }
+      }
+      );
+  }
+
   loadRegisters() {
-    this.registers = []; // Resetta i registri prima di ogni polling
-    const addresses = this.registersArray.value;
-    const requests = addresses.map((address: number) => 
-      this.modbusService.readRegister(this.ip.value as any, this.port.value as any, this.slave.value as any, address, 1)
+    console.log('Loading registers...');
+    console.log('Registers:', this.registers);
+
+    // Assicurati che this.registers contenga dei dati
+    if (!this.registers || this.registers.length === 0) {
+      console.error('No registers to load.');
+      return;
+    }
+
+    //this.registers = []; // Resetta i registri prima di ogni polling
+    const requests = this.registers.map(register =>
+      this.modbusService.readRegister(this.ip.value as any, this.port.value as any, this.slave.value as any, register.address, 1)
     );
-    return requests.forEach((request: { subscribe: (arg0: (data: any) => void) => any; }) => 
-      request.subscribe(data => {
-        this.registers.push({ address: data.register, value: data.value });
-      })
-    );
+
+    forkJoin(requests).subscribe(responses => {
+      responses.forEach(data => {
+        const register = this.registers.find(r => r.address === data.register);
+        if (register) {
+          register.value = data.value;
+        } else {
+          this.registers.push({ address: data.register, value: data.value });
+        }
+      });
+
+      // Aggiorna la struttura dataSource
+      this.dataSource = this.registers.map(reg => ({
+        registro: reg.address,
+        valore: reg.value,
+        azioni: ''
+      }));
+
+      console.log('Registers loaded:', this.registers);
+      console.log('Data source:', this.dataSource);
+    });
+  }
+
+  parseRegistersInput() {
+    // Rimuove eventuali spazi o virgole in eccesso
+    const cleanedInput = this.registersInput.replace(/\s+/g, '').replace(/,+/g, ',').replace(/^,|,$/g, '');
+
+    this.registers = cleanedInput.split(',').map(addr => {
+      const parsedAddr = parseInt(addr, 10);
+      if (isNaN(parsedAddr)) {
+        console.error('Invalid register address:', addr);
+        return null;
+      }
+      return { address: parsedAddr, value: 0 };
+    }).filter(register => register !== null);
   }
 
   submitForm() {
+    this.parseRegistersInput();
     console.log('Form submitted with values:', {
-      ip: this.ip.value,
-      port: this.port.value,
-      slave: this.slave.value,
-      registers_list: this.registersArray.value
+      ip: this.ip,
+      port: this.port,
+      slave: this.slave,
+      registers: this.registers
     });
+    this.registers.pop();
     this.loadRegisters();
+
   }
 }
